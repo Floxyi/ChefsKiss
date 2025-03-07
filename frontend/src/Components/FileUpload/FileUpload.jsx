9703e6d1509dfa5ff7992aa6d115906ded636553
nxdxnx <nadineschatz@posteo.de>
2025-03-07 15:11:17 +0100

    added testdata

--- frontend/src/Components/FileUpload/FileUpload.jsx
+++ frontend/src/Components/FileUpload/FileUpload.jsx
@@ -15,7 +15,7 @@ const FileUpload = ({ onFileChange, previews }) => {
             />
             <label
                 htmlFor="fileInput"
-                className="w-[120px] h-[90px] border-[4px] border-dashed rounded-xl border-primary-dark flex justify-center items-center relative mt-2 mr-3 cursor-pointer"
+                className="min-w-[120px] h-[90px] border-[4px] border-dashed rounded-xl border-primary-dark flex justify-center items-center relative mt-2 mr-3 cursor-pointer"
             >
                 <div className="self-center text-primary-dark">
                     <CameraIcon />
@@ -24,11 +24,11 @@ const FileUpload = ({ onFileChange, previews }) => {
                     <PlusIcon />
                 </div>
             </label>
-            <div className="flex mt-2 gap-4">
+            <div className="flex mt-2 gap-4 overflow-x-auto custom-scrollbar-no-margin mb-2">
                 {previews.length > 0 &&
                     previews.map((preview, index) => (
                         <img
-                            className="h-[90px] shadow-md rounded-lg mb-4"
+                            className="h-[90px] shadow-md rounded-lg"
                             key={index}
                             src={preview}
                             alt={`Preview ${index}`}
